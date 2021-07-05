567ca7770c986f4f138aefe0de4a07420f7cd8ab
Leonardo Christino <leomilho@gmail.com>
2021-07-05 17:25:34 -0300

    Proposal to match requirements and extend functionality
    I've used exisitng logic from the ConceptGraph plugin to help creating the line3D logic:
    * I modified the geometry creation to use surface tool
    * added 3d/2d options
    * added custom material option
    * modified the uv calculation to match the 2d or 3d expectation
    * moved the generation of points to a path, so that visual gizmos can be used such as is done with line2d
    * used a meshInstance to generate the mesh (instead of pure immediateGeometry) in order to work well with surfacetool
    * modified to a spatial shader to be easy to modify or use as an example by users
    * used vertex colors to perform the color gradient pooling or setting the default color
    * removed the color_mix for now
    * follow_camera
    * curve points as exported variable
    
    This is to be seen as a proposal. Feedbacks are welcome!

--- addons/line_3d/Line3D.gd
+++ addons/line_3d/Line3D.gd
@@ -1,159 +1,128 @@
 tool
-extends ImmediateGeometry
+extends Path
+class_name Line3D
 
-export var points = [Vector3(0,0,0), Vector3(5,0,0)]
-export var width : float = 1
-export var width_curve : Curve
+export var curve_points:PoolVector3Array setget set_points, get_points
+export var width : float = 0.1 setget set_width
+export var width_curve : Curve setget set_width_curve
 
-export var global_coords : bool = false
-export(int, "None", "Tile", "Stretch") var texture_mode = 2
-export var texture : Texture
-export var color_mix_power : float = 0.5
-export var default_color : Color = Color.white
-export var gradient : GradientTexture
+export var global_coords : bool = false setget set_global_coords
+export(int, "None", "Tile", "Stretch") var texture_mode = 2 setget set_texture_mode
+export var texture : Texture setget set_texture
+export var default_color : Color = Color.white setget set_default_color
+export var gradient : GradientTexture setget set_gradient
+export var flat : bool = false setget set_flat
+export var resolution : float = 1.0 setget set_resolution
+export var cross_section_resolution : int = 10 setget set_cross_section_resolution
+export var smooth : bool = false setget set_smooth
 
-var camera : Camera
-var camera_origin : Vector3
+export var custom_material: Material setget set_material
+export var follow_camera:=true setget set_follow_camera
 
-func _ready():
-	material_override = preload("res://addons/Line3D/Line3D_material.tres")
+var geometry = null
+var geometry_script = preload("res://addons/Line3D/MeshInstance.gd")
 
-func _process(delta):
-	if gradient == null:
-		material_override.set("shader_param/tint_power", color_mix_power)
-		material_override.set("shader_param/gradient_power", 0)
+func _enter_tree() -> void:
+	reload_geometry()
+	update()
+
+func reload_geometry():
+	for c in get_children():
+		c.free()
+	
+	curve = Curve3D.new()
+	
+	geometry = MeshInstance.new()
+	geometry.name = 'MeshInstance'
+	geometry.set_script(geometry_script)
+	geometry.connect('script_changed', self, 'update')
+	add_child(geometry)
+
+func _ready() -> void:
+	connect('curve_changed', self, 'update')
+	connect('script_changed', self, 'update')
+	update()
+	
+func update():
+	if geometry == null:
+		call_deferred('reload_geometry')
 	else:
-		material_override.set("shader_param/tint_power", 0)
-		material_override.set("shader_param/gradient_power", color_mix_power)
-		
-	if texture_mode == 0:
-		material_override.set("shader_param/tint_power", 1)
-	
-	material_override.set("shader_param/albedo", default_color)
-	material_override.set("shader_param/gradient", gradient)	
-	material_override.set("shader_param/texture_albedo", texture)
-	
-	# Prevent render if not enough points
-	if points.size() < 2:
-		clear()
-		return
-		
-	camera = get_viewport().get_camera()
-	if camera == null:
-		camera_origin = Vector3.UP	
-	else:
-		camera_origin = to_local(camera.get_global_transform().origin)
-	
-	var uv_progress_step : float = 1.0 / (points.size() - 1)
-	var uv_progress : float = 0
-	
-	var progress_step : float = 1.0 / points.size()
-	var progress : float = 0
-	
-	clear()
-	begin(Mesh.PRIMITIVE_TRIANGLES)
-	
-	for i in range(points.size() - 1):
-		var thickness_segment_start : float = width
-		var thickness_segment_end : float = width
-		var point_a : Vector3 = points[i]
-		var point_b : Vector3 = points[i+1]
-		
-		if width_curve != null:
-			thickness_segment_start = width_curve.interpolate(progress)
-			thickness_segment_end = width_curve.interpolate(progress + progress_step)
-		
-		if global_coords:
-			point_a = to_local(point_a)
-			point_b = to_local(point_b)
-	
-		var point_ab : Vector3 = point_b - point_a
-		var orth : Vector3 = (camera_origin - ((point_a + point_b) / 2)).cross(point_ab).normalized()
-		var orthogonal_ab_start : Vector3 = orth * thickness_segment_start
-		var orthogonal_ab_end : Vector3 = orth * thickness_segment_end
-		
-		var a_to_ab_start : Vector3 = point_a + orthogonal_ab_start
-		var a_from_ab_start : Vector3 = point_a - orthogonal_ab_start
-		var b_to_ab_end : Vector3 = point_b + orthogonal_ab_end
-		var b_from_ab_end : Vector3 = point_b - orthogonal_ab_end
+		geometry.call_deferred('update')
 
-		match texture_mode:
-			Line2D.LINE_TEXTURE_NONE:
-				draw_stretch(a_to_ab_start, a_from_ab_start, b_to_ab_end, b_from_ab_end, 
-							uv_progress, uv_progress_step)
-			Line2D.LINE_TEXTURE_STRETCH:
-				draw_stretch(a_to_ab_start, a_from_ab_start, b_to_ab_end, b_from_ab_end, 
-							uv_progress, uv_progress_step)
-			Line2D.LINE_TEXTURE_TILE:
-				var ab_len = point_ab.length()
-				var ab_floor = floor(ab_len)
-				var ab_frac = ab_len - ab_floor
-				
-				set_uv(Vector2(ab_floor, 0))
-				add_vertex(a_to_ab_start)
-				set_uv(Vector2(-ab_frac, 0))
-				add_vertex(b_to_ab_end)
-				set_uv(Vector2(ab_floor, 1))
-				add_vertex(a_from_ab_start)
-				set_uv(Vector2(-ab_frac, 0))
-				add_vertex(b_to_ab_end)
-				set_uv(Vector2(-ab_frac, 1))
-				add_vertex(b_from_ab_end)
-				set_uv(Vector2(ab_floor, 1))
-				add_vertex(a_from_ab_start)
-		
-		uv_progress += uv_progress_step
-		progress += progress_step
-		
-	end()	
-		
-func draw_stretch(a_to_ab_start:Vector3, a_from_ab_start:Vector3, 
-				b_to_ab_end:Vector3, b_from_ab_end:Vector3, 
-				uv_progress:float, uv_progress_step:float):
-	var zero = uv_progress
-	var one = uv_progress + uv_progress_step 
-	
-	# Triangle 1
-	set_uv(Vector2(zero, 0))
-	add_vertex(a_to_ab_start)
-	
-	set_uv(Vector2(one, 0))
-	add_vertex(b_to_ab_end)
-	
-	set_uv(Vector2(zero, 1))
-	add_vertex(a_from_ab_start)
-		
-	# Triangle 2
-	set_uv(Vector2(one, 0))
-	add_vertex(b_to_ab_end)
 
-	set_uv(Vector2(one, 1))
-	add_vertex(b_from_ab_end)
-
-	set_uv(Vector2(zero, 1))
-	add_vertex(a_from_ab_start)
-
-func add_point(position : Vector3, at_position : int = -1):
-	if at_position == -1:
-		points.append(position)
-	else:
-		points.insert(at_position, position)
-		
-func remove_point(i : int):
-	if points.size() > i:
-		points.remove(i)
-		
-func set_point_position(i : int, position : Vector3):
-	if points.size() > i:
-		points[i] = position
-		
-func clear_points():
-	points.clear()		
-
-func get_point_count():
-	return points.size()
+func set_width(v):
+	width = v
+	update()
 	
-func get_point_position(i : int) -> Vector3:
-	if points.size() > i:
-		return points[i]
-	return Vector3.ZERO
+	
+func set_width_curve(v):
+	width_curve = v
+	update()
+	
+	
+func set_global_coords(v):
+	global_coords = v
+	update()
+	
+	
+func set_texture_mode(v):
+	texture_mode = v
+	update()
+	
+	
+func set_texture(v):
+	texture = v
+	update()
+	
+	
+func set_default_color(v):
+	default_color = v
+	update()
+	
+	
+func set_gradient(v):
+	gradient = v
+	update()
+	
+	
+func set_flat(v):
+	flat = v
+	update()
+	
+	
+func set_material(v):
+	custom_material = v
+	update()
+	
+	
+func set_resolution(v):
+	resolution = v
+	update()
+	
+	
+func set_cross_section_resolution(v):
+	cross_section_resolution = v
+	update()
+	
+	
+func set_smooth(v):
+	smooth = v
+	update()
+	
+	
+func set_follow_camera(v):
+	follow_camera = v
+	update()
+	
+	
+func set_points(v):
+	for i in range(v.size()):
+		curve.set_point_position(i, v[i])
+	update()
+	
+	
+func get_points() -> PoolVector3Array:
+	var ret = PoolVector3Array()
+	for i in range(curve.get_point_count()):
+		ret.append(curve.get_point_position(i))
+	return ret
--- addons/line_3d/plugin.gd
+++ addons/line_3d/plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	add_custom_type("Line3D", "ImmediateGeometry", preload("res://addons/Line3D/Line3D.gd"),
+	add_custom_type("Line3D", "Path", preload("res://addons/Line3D/Line3D.gd"),
 	preload("res://addons/Line3D/line_3d.png"))
 
 func _exit_tree():
